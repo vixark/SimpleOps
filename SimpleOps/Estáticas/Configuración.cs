abe1c1fbbff41eb738e15ccebcb7d46e2e929cba
vixark <vixark@outlook.com>
2021-06-29 05:30:55 -0500

    29JUN21

--- SimpleOps/Estáticas/Configuración.cs
+++ SimpleOps/Estáticas/Configuración.cs
@@ -1,16 +1,26 @@
-﻿// *****************************************************************************************************************************************************
-// Importante  *****************************************************************************************************************************************
-// *****************************************************************************************************************************************************
+﻿// Copyright Notice:
 //
-// Si se hace cualquier cambio en este archivo, se generará conflicto con los usuarios del código que hayan personalizado alguno de los valores.
-// La solución para el conflicto es que el usuario rechace el cambio de sus personalizaciones en Cambios de GIT, haga Pull para incorporar los nuevos
-// cambios y cambie nuevamente los valores a sus valores personalizados.
+// SimpleOps® is a free ERP software for small businesses and independents.
+// Copyright© 2021 Vixark (vixark@outlook.com).
+// For more information about SimpleOps®, see https://simpleops.net.
 //
-// Aunque la solución es fácil, es preferible evitar hacer cambios a este archivo para que los usuarios del código no tengan que hacer ese procedimiento.
+// This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
+// General Public License as published by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
 //
-// *****************************************************************************************************************************************************
-// *****************************************************************************************************************************************************
-// *****************************************************************************************************************************************************
+// This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY, without even the
+// implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
+// License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License along with this program. If not,
+// see https://www.gnu.org/licenses.
+//
+// This License does not grant permission to use the trade names, trademarks, service marks, or product names
+// of the Licensor, except as required for reasonable and customary use in describing the origin of the Work
+// and reproducing the content of the NOTICE file.
+//
+// Removing or changing the above text is not allowed.
+//
 
 
 
--- SimpleOps/Modelo/Entidades/Usuario.cs
+++ SimpleOps/Modelo/Entidades/Usuario.cs
@@ -1,4 +1,28 @@
-﻿using System;
+﻿// Copyright Notice:
+//
+// SimpleOps® is a free ERP software for small businesses and independents.
+// Copyright© 2021 Vixark (vixark@outlook.com).
+// For more information about SimpleOps®, see https://simpleops.net.
+//
+// This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
+// General Public License as published by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY, without even the
+// implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
+// License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License along with this program. If not,
+// see https://www.gnu.org/licenses.
+//
+// This License does not grant permission to use the trade names, trademarks, service marks, or product names
+// of the Licensor, except as required for reasonable and customary use in describing the origin of the Work
+// and reproducing the content of the NOTICE file.
+//
+// Removing or changing the above text is not allowed.
+//
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
